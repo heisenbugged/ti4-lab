85114649ca1b62e0b59846035e331caec99869fa
James Strong <54325531+heisenbugged@users.noreply.github.com>
2024-07-02 14:29:55 -0500

    ensure all wekker slices have an adjacent blue

--- app/draft/wekker/sliceGenerator.ts
+++ app/draft/wekker/sliceGenerator.ts
@@ -1,9 +1,37 @@
 import { SystemId } from "~/types";
 import { generateSlices as miltyGenerateSlices } from "../milty/sliceGenerator";
 import { SLICE_SHAPES } from "../sliceShapes";
+import { systemData } from "~/data/systemData";
+import { shuffle } from "../helpers/randomization";
 
 function generateSlices(sliceCount: number, availableSystems: SystemId[]) {
-  return miltyGenerateSlices(sliceCount, availableSystems, SLICE_SHAPES.wekker);
+  const miltySlices = miltyGenerateSlices(
+    sliceCount,
+    availableSystems,
+    SLICE_SHAPES.wekker,
+  );
+
+  // ensure at least one blue adjacnet to home system,
+  miltySlices.forEach((slice) => {
+    const candidates: SystemId[] = [slice[0], slice[2]];
+    const allRed = candidates.every(
+      (systemId) => systemData[systemId].type === "RED",
+    );
+
+    if (allRed) {
+      const blueSystems = availableSystems.filter(
+        (systemId) => systemData[systemId].type === "BLUE",
+      );
+      const candidate = shuffle(blueSystems)[0];
+      const blueIdx = slice.findIndex((s) => s === candidate);
+      const redIdx = Math.random() > 0.5 ? 0 : 2;
+
+      slice[blueIdx] = candidates[redIdx];
+      slice[redIdx] = candidate;
+    }
+  });
+
+  return miltySlices;
 }
 
 export { generateSlices };
