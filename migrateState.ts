73548d39bdcc164ec741c2877be0ccd6f4a7ee93
James Strong <54325531+heisenbugged@users.noreply.github.com>
2024-10-14 07:07:59 -0500

    fix bad data

--- migrateState.ts
+++ migrateState.ts
@@ -9,27 +9,17 @@ import { DiscordData, Draft, DraftSelection, Slice, Tile } from "~/types";
 async function migrateState() {
   const records = await findDrafts();
   records.forEach(async (draft) => {
-    // parce updated-at as utc string
-    const updatedAt = new Date(draft.updatedAt);
-    // calculate how many days ago updatedAt is
-    const daysAgo = Math.floor(
-      (Date.now() - updatedAt.getTime()) / (1000 * 60 * 60 * 24),
-    );
-    console.log("daysago", daysAgo);
-    if (daysAgo > 2) {
-      const rawData = JSON.parse(draft.data as string) as Draft;
-      const doesNotMatch = rawData.players.some(
-        (player, idx) => player.name !== `Player ${idx + 1}`,
-      );
-      if (doesNotMatch) {
-        return;
-      }
-
-      console.log("we are deleting this mock draft");
-      console.log(rawData.players.map((player) => player.name).join(" "));
-
-      await db.delete(drafts).where(eq(drafts.id, draft.id));
+    const rawData = JSON.parse(draft.data as string) as Draft;
+    if (rawData.settings.gameSets !== undefined) {
+      rawData.settings.factionGameSets = rawData.settings.gameSets;
+      rawData.settings.tileGameSets = rawData.settings.gameSets;
+      delete rawData.settings.gameSets;
     }
+
+    await db
+      .update(drafts)
+      .set({ data: JSON.stringify(rawData) })
+      .where(eq(drafts.id, draft.id));
   });
 }
 
