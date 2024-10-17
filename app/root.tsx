26bf04d27d231a2614838bb1d4457ee41b3994f4
James Strong <54325531+heisenbugged@users.noreply.github.com>
2024-10-17 09:22:49 -0500

    add hotjar

--- app/root.tsx
+++ app/root.tsx
@@ -9,8 +9,6 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
-  useRouteError,
   useRouteLoaderData,
 } from "@remix-run/react";
 import {
@@ -203,6 +201,20 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
         <ColorSchemeScript defaultColorScheme="auto" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:5175222,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+          }}
+        />
       </head>
       <body>
         <MantineProvider
